430ce341b4bfb07383ba0ab18967eb6a5ff0b95b
tamaradanilova21 <roznica14@yandex.ru>
2023-07-04 17:24:23 +0400

    fix mobile

--- src/components/Navbar/ui/BurgerMenu/index.js
+++ src/components/Navbar/ui/BurgerMenu/index.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles.module.css';
 import { Link } from "react-router-dom";
-import { getAuthStatus } from '../../../../pages/AuthPage/services/slice';
+import { getAuthStatus, setAuthStatus, setToken } from '../../../../pages/AuthPage/services/slice';
 
 export const BurgerMenu = ({ isOpen, setIsOpen }) => {
 
     const authStatus = useSelector(getAuthStatus);
+
+    const dispatch = useDispatch();
+
+    const sighUp = () => {
+        setIsOpen(true)
+        localStorage.removeItem('token');
+        dispatch(setAuthStatus(false));
+        dispatch(setToken(null));
+    }
 
     return (
         <div>
@@ -47,18 +56,26 @@ export const BurgerMenu = ({ isOpen, setIsOpen }) => {
                             </Link>
                         </div>
                         {
-                            !authStatus &&
-                            <div className={styles.container_signin}>
-                                <p className={styles.text}>
-                                    Зарегистрироваться
-                                </p>
-                                <button
-                                    className={styles.button}
-                                    onClick={() => setIsOpen(true)}
-                                >
-                                    <Link className={styles.link_btn} to={'/auth'}>Войти</Link>
-                                </button>
-                            </div>
+                            !authStatus
+                                ? <div className={styles.container_signin}>
+                                    <p className={styles.text}>
+                                        Зарегистрироваться
+                                    </p>
+                                    <button
+                                        className={styles.button}
+                                        onClick={() => setIsOpen(true)}
+                                    >
+                                        <Link className={styles.link_btn} to={'/auth'}>Войти</Link>
+                                    </button>
+                                </div>
+                                : <div className={styles.container_signin}>
+                                    <button
+                                        className={styles.button}
+                                        onClick={sighUp}
+                                    >
+                                        <Link className={styles.link_btn} to={'/auth'}>Выйти</Link>
+                                    </button>
+                                </div>
                         }
                     </div>
                 }
